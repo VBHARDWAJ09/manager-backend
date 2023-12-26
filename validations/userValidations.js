a13ec2b9d04c1414fafa8609100825a7d53eb3ae
Vishal Bhardwaj <vishalbhardwaj@Etechaces.com>
2023-12-26 15:02:44 +0530

    first commit

--- validations/userValidations.js
+++ validations/userValidations.js
@@ -1,12 +1,12 @@
 const { body } = require('express-validator')
 
 module.exports.registerValidations = [
-    body('name').not().isEmpty().trim().escape().withMessage('name is required'),
-    body('email').isEmail().normalizeEmail().trim().escape().withMessage('email is required'),
+    body('name').not().isEmpty().trim().escape().withMessage('Invalid name'),
+    body('email').isEmail().normalizeEmail().trim().escape().withMessage('Invalid email'),
     body('password').isLength({ min: 5 }).withMessage('password at least 5 characters')
 ]
 
 module.exports.loginValidations = [
-    body('email').isEmail().normalizeEmail().trim().escape().withMessage('email is required'),
+    body('email').isEmail().normalizeEmail().trim().escape().withMessage('Invalid email'),
     body('password').isLength({ min: 5 }).withMessage('password at least 5 characters')
 ]
