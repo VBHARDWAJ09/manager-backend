dfc100864fd43637935fac5fbf744650d3fce497
Vishal Bhardwaj <vishalbhardwaj@Etechaces.com>
2023-12-26 15:01:16 +0530

    first commit

--- controller/user.js
+++ controller/user.js
@@ -12,15 +12,15 @@ module.exports.registerController = async (req, res) => {
             if (!userExist) {
                 const _password = await hashedPassword(password)
                 await userModal.create({ name, email, password: _password, admin: false })
-                return res.status(200).json({ message: "User registed successfully" })
+                return res.status(200).json({ messages: [{ msg: "User registed successfully" }], success: true })
             } else {
-                return res.status(203).json({ errors: [{ msg: "User Already Exist" }] })
+                return res.status(200).json({ messages: [{ msg: "User Already Exist" }], success: false })
             }
         } catch (err) {
-            return res.status(500).json({ errors: [{ msg: err.message }] })
+            return res.status(500).json({ messages: [{ msg: err.message }], success: false })
         }
     } else {
-        return res.status(400).json({ errors: filterKeysFromArray(errors.array(), ['msg']) })
+        return res.status(400).json({ messages: filterKeysFromArray(errors.array(), ['msg']), success: false })
     }
 }
 
@@ -34,18 +34,18 @@ module.exports.loginController = async (req, res) => {
                 const _match = await comparePassword(password, user.password)
                 if (_match) {
                     const token = await createToken({ id: user._id, name: user.name })
-                    return res.status(200).json({ message: "Login successfully.", token })
+                    return res.status(200).json({ messages: [{ msg: "Login successfully." }], token, success: true })
                 } else {
-                    return res.status(201).json({ errors: [{ msg: "Incorrect password" }] })
+                    return res.status(201).json({ messages: [{ msg: "Incorrect password" }], success: false })
                 }
             } else {
-                return res.status(203).json({ errors: [{ msg: "User Not Exist" }] })
+                return res.status(203).json({ messages: [{ msg: "User Not Exist" }], success: false })
             }
         } catch (err) {
             console.log(err.message)
-            return res.status(500).json({ errors: [{ msg: err.message }] })
+            return res.status(500).json({ messages: [{ msg: err.message }], success: false })
         }
     } else {
-        return res.status(400).json({ errors: filterKeysFromArray(errors.array(), ['msg']) })
+        return res.status(400).json({ messages: filterKeysFromArray(errors.array(), ['msg']), success: false })
     }
 }
